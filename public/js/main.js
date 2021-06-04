8b92a22c25fbe21d10319b60c42cab57a655a73c
ElsongMaster <elsongmaster@gmail.com>
2021-06-04 17:09:28 +0200

    Partie sur les bordures

--- public/js/main.js
+++ public/js/main.js
@@ -14,3 +14,30 @@ divStyleColor.addEventListener("click", (e) => {
     functStyleColor(e.target);
   }
 });
+
+let divStyleBorder = document.querySelector(".styleBorder");
+let tabStyleBorder = Array.from(divStyleBorder.children);
+let functBorderStyle = (node) => {
+  if (node == tabStyleBorder[0]) {
+    console.log(resultDiv.style.backgroundColor);
+    node.addEventListener("focusout", () => {
+      resultDiv.style.backgroundColor = `${node.value}`;
+    });
+  } else if (node == tabStyleBorder[1]) {
+    resultDiv.style.border = `${node.value}px solid black`;
+  } else if (node == tabStyleBorder[2]) {
+    resultDiv.style.borderTop = `${node.value}px solid black`;
+  } else if (node == tabStyleBorder[3]) {
+    resultDiv.setAttribute("style", `border-right:${node.value}px solid black`);
+  } else if (node == tabStyleBorder[4]) {
+    resultDiv.style.borderBottom = `${node.value}px solid black`;
+  } else if (node == tabStyleBorder[5]) {
+    resultDiv.style.borderLeft = `${node.value}px solid black`;
+  }
+};
+
+divStyleBorder.addEventListener("click", (e) => {
+  if (tabStyleBorder.includes(e.target)) {
+    functBorderStyle(e.target);
+  }
+});
